cac2cd86d81f0bc673201e0319210f50a81fff27
sakaguchi2783 <sakaguchi@meiko-p.co.jp>
2026-01-30 12:43:27 +0900

    修正2

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL='https://kajzoimjgstpibpjzzys.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthanpvaW1qZ3N0cGlicGp6enlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyODY0MjcsImV4cCI6MjA4Mzg2MjQyN30.Qc8_q-6g3O1Q9rAxMNLgA6LJ8_roB42kpjjc9KfX4ng';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing VITE_SUPABASE_URL / VITE_SUPABASE_ANON_KEY');
